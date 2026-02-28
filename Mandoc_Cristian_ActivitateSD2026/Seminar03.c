ed12287ca37b3eae4c16b157fead365a2a9b8f51
cristian andrei mandoc <andreimandoc10@gmail.com>
2026-02-28 15:25:25 +0200

    Am adaugat fisierul seminar03.c cu citirea variabilei reale

---
+++ Mandoc_Cristian_ActivitateSD2026/Seminar03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+
+{
+	printf("Introduceti un numar real: ");
+	float variabila = 0.0;
+	scanf_s("%f", &variabila);
+	printf("Ai introdus: %5.2f", variabila);
+	 
+}
