f1bd7fbd43fe8dbf448505bbdcda880fc204e896
cristian andrei mandoc <andreimandoc10@gmail.com>
2026-03-08 17:23:06 +0200

    Laborator 2 lucrat

--- Mandoc_Cristian_ActivitateSD2026/LucruSupl1.c
+++ Mandoc_Cristian_ActivitateSD2026/LucruSupl1.c
@@ -1,59 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-struct Vehicul {
-	int id;
-	int putereCp;
-	char* marca;
-	float pret;
-	char seria;
-};
-
-struct Vehicul initializare(int id, int putereCp, const char* marca, float pret, char seria) {
-
-	struct Vehicul v;
-	v.id = id;
-	v.putereCp = putereCp;
-	v.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1)) ;
-	strcpy_s(v.marca, strlen(marca) + 1, marca);
-	v.pret = pret;
-	v.seria = seria;
-	return v;
-}
-
-void afisare(struct Vehicul v) {
-	if (v.marca != NULL) 
-		printf("%d Vehiculul %s cu seria %c are %d cai putere si pretul de %2.5f./n ", v.id, v.marca, v.seria, v.putereCp, v.pret);
-	else
-		printf("%d Vehiculul cu seria %c are %d cai putere si pretul de %2.5f./n ", v.id, v.seria, v.putereCp, v.pret);
-	
-}
-
-void modificarePret(struct Vehicul* v, float pretNou) {
-	if (pretNou > 0) {
-		v->pret = pretNou;
-	}
-}
-
-void dezalocare(struct Vehicul* v) {
-	if (v->marca != NULL) {
-		free(v->marca);
-		v->marca = NULL;
-	}
-}
-
-
-int main()
-{
-	struct Vehicul v;
-	v = initializare(1, 150, "Audi", 20345.3, 'A');
-	afisare(v);
-	modificarePret(&v, 18997.5);
-	afisare(v);
-	dezalocare(&v);
-	afisare(v);
-
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//struct Student {
+//	int id;
+//	int grupa;
+//	char* nume;
+//	float medie;
+//	char serie;
+//};
+//
+//struct Student initializare(int id, int grupa, const char* nume, float medie, char serie) {
+//
+//	struct Student s;
+//	s.id = id;
+//	s.grupa = grupa;
+//	s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//	strcpy_s(s.nume, strlen(nume) + 1, nume);
+//	s.medie = medie;
+//	s.serie = serie;
+//
+//	return s;
+//}
+//
+//void afisare(struct Student s) {
+//	if (s.nume != NULL)
+//		printf("%d Stdentul %s din grupa %d, seria %c, are media %2.5f.\n", s.id, s.nume, s.grupa, s.serie, s.medie);
+//	else
+//		printf("%d Stdentul din grupa %d, seria %c, are media %2.5f.\n", s.id, s.grupa, s.serie, s.medie);
+//}
+//
+//void modificareMedie(struct Student* s, float medieNoua) {
+//	if (medieNoua > 0)
+//		s->medie = medieNoua;
+//}
+//
+//void dezalocare(struct Student* s) {
+//	if (s->nume != NULL) {
+//		free(s->nume);
+//		s->nume = NULL;
+//	}
+//}
+//
+//int main()
+//{
+//	struct Student s;
+//	s = initializare(1, 8402, "Mandoc Andrei", 8.6, 'A');
+//	afisare(s);
+//	modificareMedie(&s, 9.6);
+//	afisare(s);
+//	dezalocare(&s);
+//	afisare(s);
+//
+//	return 0;
+//}
