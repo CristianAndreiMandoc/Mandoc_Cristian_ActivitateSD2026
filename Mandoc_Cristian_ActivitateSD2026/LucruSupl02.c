f412bf6290a0c115ed1021565f76fd8072412e94
cristian andrei mandoc <andreimandoc10@gmail.com>
2026-03-22 23:50:15 +0200

    .

--- Mandoc_Cristian_ActivitateSD2026/LucruSupl02.c
+++ Mandoc_Cristian_ActivitateSD2026/LucruSupl02.c
@@ -1,53 +1,53 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-struct Vehicul {
-	int id;
-	int puterecp;
-	char* marca;
-	float pret;
-	char segment;
-};
-
-struct Vehicul initializare(int id, int puterecp, const char* marca, float pret, char segment) {
-	struct Vehicul v;
-	v.id = id;
-	v.puterecp = puterecp;
-	v.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1));
-	strcpy_s(v.marca, strlen(marca) + 1, marca);
-	v.pret = pret;
-	v.segment = segment;
-
-	return v;
-}
-
-void afisare(struct Vehicul v) {
-	if (v.marca != NULL)
-		printf("Vehiculul cu ID %d, marca %s si segment de piata %c are o putere de %d cai putere si pret de %2.2f euro.\n", v.id, v.marca, v.segment, v.puterecp, v.pret);
-	else
-		printf("Vehiculul cu ID %d si segment de piata %c are o putere de %d cai putere si pret de %2.2f euro.\n", v.id, v.segment, v.puterecp, v.pret);
-}
-
-void afisareVector(struct Vehicul* vector, int nrElemente) {
-	for (int i=0; i < nrElemente; i++) {
-		afisare(vector[i]);
-	}
-}
-
- 
-
-
-int main()
-{
-	struct Vehicul* vehicule = NULL;
-	int nrVehicule = 3;
-	vehicule = (struct Vehicule*)malloc(sizeof(struct Vehicul) * nrVehicule);
-	vehicule[0] = initializare(1, 150, "BMW", 19000.00, 'E');
-	vehicule[1] = initializare(2, 120, "Volkswagen", 8000.00, 'D');
-	vehicule[2] = initializare(3, 200, "Mercedes", 40000.00, 'E');
-
-	afisareVector(vehicule, nrVehicule);
-	
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//struct Vehicul {
+//	int id;
+//	int puterecp;
+//	char* marca;
+//	float pret;
+//	char segment;
+//};
+//
+//struct Vehicul initializare(int id, int puterecp, const char* marca, float pret, char segment) {
+//	struct Vehicul v;
+//	v.id = id;
+//	v.puterecp = puterecp;
+//	v.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1));
+//	strcpy_s(v.marca, strlen(marca) + 1, marca);
+//	v.pret = pret;
+//	v.segment = segment;
+//
+//	return v;
+//}
+//
+//void afisare(struct Vehicul v) {
+//	if (v.marca != NULL)
+//		printf("Vehiculul cu ID %d, marca %s si segment de piata %c are o putere de %d cai putere si pret de %2.2f euro.\n", v.id, v.marca, v.segment, v.puterecp, v.pret);
+//	else
+//		printf("Vehiculul cu ID %d si segment de piata %c are o putere de %d cai putere si pret de %2.2f euro.\n", v.id, v.segment, v.puterecp, v.pret);
+//}
+//
+//void afisareVector(struct Vehicul* vector, int nrElemente) {
+//	for (int i=0; i < nrElemente; i++) {
+//		afisare(vector[i]);
+//	}
+//}
+//
+// 
+//
+//
+//int main()
+//{
+//	struct Vehicul* vehicule = NULL;
+//	int nrVehicule = 3;
+//	vehicule = (struct Vehicule*)malloc(sizeof(struct Vehicul) * nrVehicule);
+//	vehicule[0] = initializare(1, 150, "BMW", 19000.00, 'E');
+//	vehicule[1] = initializare(2, 120, "Volkswagen", 8000.00, 'D');
+//	vehicule[2] = initializare(3, 200, "Mercedes", 40000.00, 'E');
+//
+//	afisareVector(vehicule, nrVehicule);
+//	
+//	return 0;
+//}
---
+++ Mandoc_Cristian_ActivitateSD2026/LucruSupl3.c
@@ -0,0 +1,130 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char marca[50];
+    char model[50];
+    int an_fabricatie;
+    float pret;
+} Masina;
+
+typedef struct Nod {
+    Masina date;
+    struct Nod* prev;
+    struct Nod* next;
+} Nod;
+
+Nod* creareNod(Masina m) {
+    Nod* nou = (Nod*)malloc(sizeof(Nod));
+    if (nou == NULL) {
+        exit(1);
+    }
+    nou->date = m;
+    nou->prev = NULL;
+    nou->next = NULL;
+    return nou;
+}
+
+void inserareFinal(Nod** cap, Nod** coada, Masina m) {
+    Nod* nou = creareNod(m);
+    if (*cap == NULL) {
+        *cap = nou;
+        *coada = nou;
+    }
+    else {
+        (*coada)->next = nou;
+        nou->prev = *coada;
+        *coada = nou;
+    }
+}
+
+void afisareInversa(Nod* coada) {
+    Nod* temp = coada;
+    printf("--- Afisare masini (de la coada la cap) ---\n");
+    while (temp != NULL) {
+        printf("ID: %d | %s %s | An: %d | Pret: %.2f\n",
+            temp->date.id, temp->date.marca, temp->date.model,
+            temp->date.an_fabricatie, temp->date.pret);
+        temp = temp->prev;
+    }
+    printf("-------------------------------------------\n");
+}
+
+int citireDinFisier(const char* numeFisier, Masina vector[], int maxElemente) {
+    FILE* fisier = fopen(numeFisier, "r");
+    if (fisier == NULL) {
+        return -1;
+    }
+
+    int n = 0;
+    while (n < maxElemente && fscanf(fisier, "%d %49s %49s %d %f",
+        &vector[n].id, vector[n].marca, vector[n].model,
+        &vector[n].an_fabricatie, &vector[n].pret) == 5) {
+        n++;
+    }
+
+    fclose(fisier);
+    return n;
+}
+
+void filtrareSiScriereInFisier(const char* numeFisier, Nod* cap, int anMinim) {
+    FILE* fisier = fopen(numeFisier, "w");
+    if (fisier == NULL) {
+        return;
+    }
+
+    Nod* temp = cap;
+    fprintf(fisier, "Masini fabricate dupa anul %d:\n\n", anMinim);
+
+    while (temp != NULL) {
+        if (temp->date.an_fabricatie > anMinim) {
+            fprintf(fisier, "%d %s %s %d %.2f\n",
+                temp->date.id, temp->date.marca, temp->date.model,
+                temp->date.an_fabricatie, temp->date.pret);
+        }
+        temp = temp->next;
+    }
+
+    fclose(fisier);
+}
+
+void distrugereLista(Nod** cap, Nod** coada) {
+    Nod* temp = *cap;
+    Nod* urmator;
+    while (temp != NULL) {
+        urmator = temp->next;
+        free(temp);
+        temp = urmator;
+    }
+    *cap = NULL;
+    *coada = NULL;
+}
+
+int main() {
+    Masina parcAuto[100];
+    Nod* capLista = NULL;
+    Nod* coadaLista = NULL;
+    int numarMasini = 0;
+
+    numarMasini = citireDinFisier("masini_in.txt", parcAuto, 100);
+
+    if (numarMasini == -1) {
+        printf("Nu s-a putut deschide fisierul masini_in.txt\n");
+        return 1;
+    }
+
+    for (int i = 0; i < numarMasini; i++) {
+        inserareFinal(&capLista, &coadaLista, parcAuto[i]);
+    }
+
+    afisareInversa(coadaLista);
+
+    filtrareSiScriereInFisier("masini_noi_out.txt", capLista, 2015);
+
+    distrugereLista(&capLista, &coadaLista);
+
+    return 0;
+}
