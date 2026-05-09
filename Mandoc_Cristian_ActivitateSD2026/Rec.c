1cd2a3b7bfe1b5cb3715d8a47ea265c4953f1351
cristian andrei mandoc <andreimandoc10@gmail.com>
2026-05-09 22:11:31 +0300

    Am recapitulatPregatire pentru lucrare

---
+++ Mandoc_Cristian_ActivitateSD2026/Rec.c
@@ -0,0 +1,61 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//typedef struct {
+//    int id;
+//    char nume[50];
+//    int salariu;
+//} Angajat;
+//
+//typedef struct {
+//    Angajat* arr;
+//    int size;
+//} Heap;
+//
+//void insertHeap(Heap* h, Angajat a) {
+//    int i = h->size++;
+//    int p = (i - 1) / 2;
+//    h->arr[i] = a;
+//    while (i > 0 && h->arr[i].salariu > h->arr[p].salariu) {
+//        Angajat aux = h->arr[i]; h->arr[i] = h->arr[p]; h->arr[p] = aux;
+//        i = p; p = (i - 1) / 2;
+//    }
+//}
+//
+//Angajat extractHeap(Heap* h) {
+//    Angajat max = h->arr[0];
+//    h->arr[0] = h->arr[--h->size];
+//    int i = 0, st, dr, m_idx;
+//    while (1) {
+//        st = 2 * i + 1; dr = 2 * i + 2; m_idx = i;
+//        if (st < h->size && h->arr[st].salariu > h->arr[m_idx].salariu) m_idx = st;
+//        if (dr < h->size && h->arr[dr].salariu > h->arr[m_idx].salariu) m_idx = dr;
+//        if (m_idx == i) break;
+//        Angajat aux = h->arr[i]; h->arr[i] = h->arr[m_idx]; h->arr[m_idx] = aux;
+//        i = m_idx;
+//    }
+//    return max;
+//}
+//
+//int main() {
+//    FILE* fin = fopen("angajati.txt", "r");
+//    FILE* fout = fopen("raport.txt", "w");
+//    Angajat a;
+//    Heap hp; hp.size = 0; hp.arr = (Angajat*)malloc(100 * sizeof(Angajat));
+//
+//    if (!fin || !fout) return 1;
+//
+//    while (fscanf(fin, "%d %s %d", &a.id, a.nume, &a.salariu) == 3) {
+//        insertHeap(&hp, a);
+//    }
+//
+//    fprintf(fout, "--- EXTRAGERE MAX-HEAP (Top Salarii) ---\n");
+//    while (hp.size > 0) {
+//        a = extractHeap(&hp);
+//        fprintf(fout, "%s -> %d RON\n", a.nume, a.salariu);
+//    }
+//
+//    free(hp.arr); fclose(fin); fclose(fout);
+//    return 0;
+//}
